0deedce3996b4cb87b5d56caaab83003b7368c22
gravity51422-dot <gravity51422@gmail.com>
2025-12-26 21:42:17 +0900

    Update player.js

--- root/player.js
+++ root/player.js
@@ -1,1 +1,34 @@
+class Player {
+  constructor(x, y, color, controls, reverse = false) {
+    this.x = x;
+    this.y = y;
+    this.size = 30;
+    this.speed = 4;
+    this.color = color;
+    this.controls = controls;
+    this.reverse = reverse;
+  }
+
+  move(keys) {
+    let dir = this.reverse ? -1 : 1;
+
+    if (keys[this.controls.left]) {
+      this.x -= this.speed * dir;
+    }
+    if (keys[this.controls.right]) {
+      this.x += this.speed * dir;
+    }
+    if (keys[this.controls.up]) {
+      this.y -= this.speed;
+    }
+    if (keys[this.controls.down]) {
+      this.y += this.speed;
+    }
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+  }
+}
 
